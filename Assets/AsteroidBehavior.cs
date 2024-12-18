bad9924c50090e98e2898787345783f738765403
kayuyu.mwaura <kayuyu.mwaura@EHR-03>
2024-12-18 11:50:50 +0300

    Imported assets to make the game universe more alive. Used this link: https://kenney.nl/assets?search=space+shooter&sort=update Did some code refactoring on Asteroid Behavior for DestroyAsteroid code reuse. This was for destroying the asteroid when it hits the earth.

--- Assets/AsteroidBehavior.cs
+++ Assets/AsteroidBehavior.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AsteroidBehavior : MonoBehaviour
 {
     public GameObject asteroidFX;
-
+    GameObject spawnedFX = null;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -16,19 +16,27 @@ public class AsteroidBehavior : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        GameObject spawnedFX = null;
+        
         if (collision.CompareTag("Laser"))
         {
-            spawnedFX = Instantiate(asteroidFX,transform.position, Quaternion.identity); //dictating how the asteroid spawns when destroyed, i.e, position
             GameManager.instance.IncrementScore();
-            Destroy(gameObject);
+            DestroyAsteroid();
         }
         if (collision.CompareTag("AsteroidDestroyer"))
         {
             Destroy(gameObject);
         }
+        if (collision.CompareTag("Earth"))
+        {
+            Debug.Log("Earth Hit");
+            DestroyAsteroid();
+        }
         Destroy(spawnedFX, 1.5f);
     }
 
-
+    private void DestroyAsteroid()
+    {
+        spawnedFX = Instantiate(asteroidFX, transform.position, Quaternion.identity); //dictating how the asteroid spawns when destroyed, i.e, position
+        Destroy(gameObject);
+    }
 }
